a8a66fbf1d8842a6cb046a0b59183f5690570fee
OsamaQHasan <106831935+OsamaQHasan@users.noreply.github.com>
2025-02-27 16:14:15 +0400

    Added a score and combo system
    Added a system that keeps track of score, and another one that keeps track of combos (streaks) which multiplies the score when the player doesn't make mistakes.

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
@@ -8,6 +9,9 @@ public class GameController : MonoBehaviour
     private BoardGenerator boardGenerator;
     [SerializeField] int x, y;
     [SerializeField] float showCardFor;
+    int combo = 0,score = 0;
+    [SerializeField] TMP_Text scoreText, comboText, levelText;
+    [SerializeField] int scorePerMatch;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,13 +31,28 @@ public class GameController : MonoBehaviour
             {
                 selectedCard1.StartFlipCard(showCardFor);
                 selectedCard2.StartFlipCard(showCardFor);
+                UpdateCombo(0);
             }
             else
             {
                 selectedCard1.CardMatched(showCardFor);
                 selectedCard2.CardMatched(showCardFor);
+                UpdateScore(score + (combo == 0 ? 1 : combo) * scorePerMatch);
+                UpdateCombo(combo + 1);
+                
             }
         }
+    }
+    private void UpdateScore(int score)
+    {
+        this.score = score;
+        scoreText.text = "SCORE: " + score;
+
+    }
+    private void UpdateCombo(int combo)
+    {
+        this.combo = combo;
+        comboText.text = "COMBO: " + combo;
     }
     public void SelectCard(Card card)
     {
