0c774f53a97848ddfe6166a4d164a44f7d46018c
OsamaQHasan <106831935+OsamaQHasan@users.noreply.github.com>
2025-02-27 12:11:20 +0400

    scalable grid
    Made the grid scale with screen height.

--- Scripts/BoardGenerator.cs
+++ Scripts/BoardGenerator.cs
@@ -6,6 +6,9 @@ public class BoardGenerator : MonoBehaviour
 {
     [SerializeField] GameObject cardPrefab, grid;
     [SerializeField] int x, y;
+    [SerializeField] float scaleDifference;
+    [SerializeField] int normalY;
+    public float scaleRatio;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +21,12 @@ public class BoardGenerator : MonoBehaviour
         
     }
 
-    void SetUpBoard() { 
-        
+    void SetUpBoard() {
+        scaleRatio = ((float)normalY /(float)y) * scaleDifference;
         RectTransform rect = grid.GetComponent<RectTransform>();
-        rect.localPosition = new Vector3(rect.localPosition.x, (y - 1) * 50);
+        rect.localPosition = new Vector3(rect.localPosition.x, (y - 1) * 50 * scaleRatio);
         rect.sizeDelta = new Vector2(x * 100, 100);
+        rect.localScale = new Vector2(rect.localScale.x * scaleRatio, rect.localScale.y * scaleRatio);
         for (int i = 0; i < x * y; i++)
         {
             Instantiate(cardPrefab, grid.transform);
