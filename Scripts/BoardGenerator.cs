7c5a6bc3a784cfb89290d30272908e5e9611a989
OsamaQHasan <106831935+OsamaQHasan@users.noreply.github.com>
2025-02-27 11:37:43 +0400

    Created the board generator
    Created a script to generate a board of cards using the card prefab.

--- Scripts/BoardGenerator.cs
+++ Scripts/BoardGenerator.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class BoardGenerator : MonoBehaviour
 {
+    [SerializeField] GameObject cardPrefab, grid;
+    [SerializeField] int x, y;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpBoard();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetUpBoard() { 
+        
+        RectTransform rect = grid.GetComponent<RectTransform>();
+        rect.localPosition = new Vector3(rect.localPosition.x, (y - 1) * 50);
+        rect.sizeDelta = new Vector2(x * 100, 100);
+        for (int i = 0; i < x * y; i++)
+        {
+            Instantiate(cardPrefab, grid.transform);
+        }
     }
 }
